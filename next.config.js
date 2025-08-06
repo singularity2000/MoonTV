a0b5724fbeef2b25d670d159e9b7d036a438b6d8
singularity2000 <61232355+singularity2000@users.noreply.github.com>
2025-08-06 23:44:50 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 /* eslint-disable @typescript-eslint/no-var-requires */
 const nextConfig = {
-  output: 'export',
+  output: 'standalone',
   eslint: {
     dirs: ['src'],
   },
