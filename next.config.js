f1cd352207b231a1c3b95f04b9f07774effae165
singularity2000 <61232355+singularity2000@users.noreply.github.com>
2025-08-06 23:12:07 +0800

    Update next.config.js
    将 output: 'standalone' 改回 output: 'export'

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 /* eslint-disable @typescript-eslint/no-var-requires */
 const nextConfig = {
-  output: 'standalone',
+  output: 'export',
   eslint: {
     dirs: ['src'],
   },
